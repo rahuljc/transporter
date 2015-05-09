bdc81ccb245b49c67212e5de650075744f368d61
rahuljc <rahul.jayc@gmail.com>
2015-05-09 16:26:38 +0530

    Added request model and mig script

---
+++ db/migrate/20150509104832_create_requests.rb
@@ -0,0 +1,14 @@
+class CreateRequests < ActiveRecord::Migration
+  def change
+	create_table :requests do |t|
+      t.integer :requester_id
+      t.integer :device_id
+      t.integer :pickup_location_id
+      t.integer :drop_location_id
+      t.decimal :distance, :precision => 15, :scale => 2, :default => 0.0
+      t.string  :status
+      t.text    :nearest_deliverers
+      t.timestamps
+    end
+  end
+end
